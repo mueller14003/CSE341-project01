cefebf04791fc678c04f9a4f6b0793b5a50da3db
mueller14003 <kyle.mueller.ghs@gmail.com>
2021-06-01 03:36:39 -0600

    Probably definitely maybe fixed now

--- app.js
+++ app.js
@@ -11,12 +11,12 @@ const csrf = require('csurf');
 const flash = require('connect-flash');
 
 const fs = require('fs');
-const https = require('https');
+// const https = require('https');
 
-const httpsOptions = {
-  key: fs.readFileSync('./key.pem'),
-  cert: fs.readFileSync('./cert.pem')
-};
+// const httpsOptions = {
+//   key: fs.readFileSync('./key.pem'),
+//   cert: fs.readFileSync('./cert.pem')
+// };
 
 const errorController = require('./controllers/error');
 const User = require('./models/user')
